fc070a07db5c22aaef573bb5d8311da14fc2baeb
DavidD121 <daviddanielian3@gmail.com>
2021-09-14 21:48:29 -0400

    Updates

---
+++ precache-manifest.920a7ae19edda28b1f10cf798cbe5179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1191c03687551884ca10485a99c25c",
+    "url": "/ReactJS-Spotify-Playlist-Analyzer/index.html"
+  },
+  {
+    "revision": "52277a8e2634aa962f7a",
+    "url": "/ReactJS-Spotify-Playlist-Analyzer/static/css/main.f3e89206.chunk.css"
+  },
+  {
+    "revision": "b0613d53d04e36b6b1f5",
+    "url": "/ReactJS-Spotify-Playlist-Analyzer/static/js/2.5c47601b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-Spotify-Playlist-Analyzer/static/js/2.5c47601b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52277a8e2634aa962f7a",
+    "url": "/ReactJS-Spotify-Playlist-Analyzer/static/js/main.85750b0c.chunk.js"
+  },
+  {
+    "revision": "e1aac5ab41b3daa9334a",
+    "url": "/ReactJS-Spotify-Playlist-Analyzer/static/js/runtime-main.4981d367.js"
+  },
+  {
+    "revision": "070e65fbf10f1af884ff9d21e6900719",
+    "url": "/ReactJS-Spotify-Playlist-Analyzer/static/media/placeholder.070e65fb.png"
+  }
+]);
